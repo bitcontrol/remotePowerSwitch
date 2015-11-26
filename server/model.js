bc0699afc15eb8fd2de2d860b54168b83b538eeb
Andreas Isenegger <andreas.isenegger@bitcontrol.ch>
2015-11-26 18:15:51 +0100

    Update schemas with latest design, improve comments.

--- server/model.js
+++ server/model.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 
-// Create schema for switch (written to by user)
+// Create schema for switch (written to by browsers, read by embedded computers)
 var switchSchema = mongoose.Schema({
-  date: Date,
-  state: Boolean
-});
-// Compile schema into model
-// Note: the name 'switch' can't be used since it is a keyword;
-// thus the name 'switchOffOn' is being used instead
-exports.switchOffOn = mongoose.model('switchOffOn', switchSchema);
-
-// Create schema for status (written to by embedded computer)
-var statusSchema = mongoose.Schema({
+  devId: String, // A string offers more possibilities than a number
   date: Date,
   state: String
 });
 // Compile schema into model
-exports.status = mongoose.model('status', switchSchema);
+exports.switches = mongoose.model('switches', switchSchema);
+
+// Create schema for status (written to by embedded computers, read by browsers)
+var statusSchema = mongoose.Schema({
+  devId: String, // A string offers more possibilities than a number
+  date: Date,
+  state: String
+});
+// Compile schema into model
+exports.status = mongoose.model('status', statusSchema);
